74d5f52409c9105ed535bb58f43618b1e97d9e1a
mattjrumble <mattjrumble@gmail.com>
2022-10-29 00:33:13 +0100

    Account for server lag and simplify likelihoods logic

--- server/likelihoods.py
+++ server/likelihoods.py
@@ -8,6 +8,8 @@ from server.locations import LOCATIONS
 from server.database import select_sightings, remove_sighting
 
 FIFTEEN_MINUTES = 15 * 60
+ESTIMATED_SERVER_LAG_RATE = 5 / FIFTEEN_MINUTES  # i.e. assume up to 5 seconds of server lag every 15 minutes
+SERVER_LAG_LIMIT = FIFTEEN_MINUTES
 logger = getLogger('uvicorn')
 
 
@@ -16,39 +18,75 @@ class Sighting:
         self.row_id = row_id
         self.location = location
         self.dt = datetime.fromisoformat(dt_string)
+        seconds_since_sighting = int((datetime.now() - self.dt).total_seconds())
+        start_of_window = (self.location * FIFTEEN_MINUTES) + seconds_since_sighting
+        server_lag = max(seconds_since_sighting * ESTIMATED_SERVER_LAG_RATE, SERVER_LAG_LIMIT)
+        self.window = RotationWindow(start_of_window, start_of_window + FIFTEEN_MINUTES + server_lag)
 
     def __repr__(self):
         return f'(Location {self.location} at {self.dt})'
 
 
-class InconsistentSections(Exception):
+class WindowsDoNotOverlap(Exception):
     pass
 
 
-class Section:
+class RotationWindow:
     """
-    Represent a section of a ring (i.e. the set of numbers modulus LENGTH). In this case, LENGTH is the number of
-    seconds it takes for a full crop circle rotation (i.e. for the crop circle to get back to where it started).
-    Assume the length of each section is < LENGTH / 2 (i.e. we don't have to worry about sections overlapping in two
-    different places).
+    Represent a window in the crop circle rotation. This can be thought of as a section of a ring of CIRCUMFERENCE
+    equal to 15 minutes * the number of crop circle locations. The start of the ring is when the crop circle moves
+    to location 0.
+
+    Assume the length of any rotation window is < LENGTH / 2 (i.e. we don't have to worry about sections overlapping
+    in two different places).
     """
-    LENGTH = len(LOCATIONS) * FIFTEEN_MINUTES
+    CIRCUMFERENCE = len(LOCATIONS) * FIFTEEN_MINUTES
 
     def __init__(self, start, end):
-        self.start = start % self.LENGTH
-        self.end = end % self.LENGTH
+        self.start = int(start % self.CIRCUMFERENCE)
+        self.end = int(end % self.CIRCUMFERENCE)
 
-    def combine(self, other_section):
+    def __len__(self):
+        if self.end > self.start:
+            return self.end - self.start
+        else:
+            return (self.CIRCUMFERENCE - self.end) + self.start
+
+    def get_likelihoods(self) -> dict[int: float]:
         """
-        Combine this section with another section to return the (smaller) overlapping section, or raise an
-        InconsistentSections error if the two sections don't overlap.
+        Return a dictionary of locations to likelihoods that the crop circle is in that particular location, based
+        on this window.
         """
-        # Offset both sections so that this section's (section 1) start is at 0. This simplifies the logic.
-        offset = self.LENGTH - self.start
+        start_location, start_offset = divmod(self.start, FIFTEEN_MINUTES)
+        end_location, end_offset = divmod(self.end, FIFTEEN_MINUTES)
+
+        if start_location == end_location:
+            likelihoods = {start_location: 1.0}
+        elif (start_location + 1) % len(LOCATIONS) == end_location:
+            likelihoods = {
+                start_location: (FIFTEEN_MINUTES - start_offset) / len(self),
+                end_location: end_offset / len(self)
+            }
+        else:
+            likelihoods = {start_location: (FIFTEEN_MINUTES - start_offset) / len(self)}
+            location = start_location
+            while location != end_location:
+                location = (location + 1) % len(LOCATIONS)
+                likelihoods[location] = FIFTEEN_MINUTES / len(self)
+            likelihoods[end_location] = end_offset / len(self)
+        return dict(sorted(likelihoods.items()))
+
+    def combine(self, other_window):
+        """
+        Combine this window with another window to return the (smaller) overlapping window, or raise an
+        WindowsDoNotOverlap error if the two window don't overlap.
+        """
+        # Offset both windows so that this windows's start is at 0. This simplifies the logic.
+        offset = self.CIRCUMFERENCE - self.start
         start = 0
-        end = (self.end + offset) % self.LENGTH
-        other_start = (other_section.start + offset) % self.LENGTH
-        other_end = (other_section.end + offset) % self.LENGTH
+        end = (self.end + offset) % self.CIRCUMFERENCE
+        other_start = (other_window.start + offset) % self.CIRCUMFERENCE
+        other_end = (other_window.end + offset) % self.CIRCUMFERENCE
 
         if other_start < end:
             start = other_start
@@ -58,10 +96,10 @@ class Section:
             if other_start < other_end:
                 start = other_start
         else:
-            raise InconsistentSections()
+            raise WindowsDoNotOverlap()
 
         # Undo the offset before returning
-        return Section(start-offset, end-offset)
+        return RotationWindow(start-offset, end-offset)
 
 
 async def recalculate():
@@ -70,12 +108,12 @@ async def recalculate():
     sightings_by_world = await get_sightings()
     likelihoods = {}
     for world, sightings in sightings_by_world.items():
-        new_sightings, old_sightings = separate_sightings(sightings)
+        window, old_sightings = separate_sightings(sightings)
+        likelihoods[world] = window.get_likelihoods()
         if old_sightings:
             logger.info('Removing old sightings for world %s: %s', world, old_sightings)
             for sighting in old_sightings:
                 await remove_sighting(sighting.row_id)
-        likelihoods[world] = get_likelihoods(new_sightings)
     logger.info('Updated likelihoods: %s', likelihoods)
     CACHE['likelihoods'] = likelihoods
     end = time()
@@ -89,72 +127,27 @@ async def get_sightings() -> dict[int, list[Sighting]]:
     return sightings_by_world
 
 
-def separate_sightings(sightings: list[Sighting]) -> tuple[list[Sighting], list[Sighting]]:
+def separate_sightings(sightings: list[Sighting]) -> tuple[RotationWindow, list[Sighting]]:
     """
-    Separate out the new, consistent sightings from the old, inconsistent ones. Favour the most recent sightings.
+    Separate out the new, consistent sightings from the old/inconsistent ones. Favour the most recent sightings.
+    Return a rotation window from the combined consistent sightings, and a list of the old sightings.
     """
-    # Go through each sighting, starting with the most recent. Each sighting gives a window of times where the crop
-    # circle will move to Location 0. Keep combining these sections until one is inconsistent.
+    # Go through each sighting, starting with the most recent. Each sighting gives a rotation window of where the
+    # crop circle currently is. Keep combining these windows until one doesn't overlap.
     new_sightings, old_sightings = [], []
     consistent = True
-    most_accurate_section = None
+    most_accurate_window = None
     for sighting in sorted(sightings, key=(lambda s: s.dt), reverse=True):
         if consistent:
-            seconds_until_location_0 = ((len(LOCATIONS) - sighting.location) % len(LOCATIONS)) * FIFTEEN_MINUTES
-            section = Section(
-                sighting.dt.timestamp() + seconds_until_location_0 - FIFTEEN_MINUTES,
-                sighting.dt.timestamp() + seconds_until_location_0 + FIFTEEN_MINUTES
-            )
-            if most_accurate_section is None:
-                most_accurate_section = section
+            if most_accurate_window is None:
+                most_accurate_window = sighting.window
             else:
                 try:
-                    most_accurate_section = most_accurate_section.combine(section)
-                except InconsistentSections:
+                    most_accurate_window = most_accurate_window.combine(sighting.window)
+                except WindowsDoNotOverlap:
                     consistent = False
-
         if consistent:
             new_sightings.append(sighting)
         else:
             old_sightings.append(sighting)
-    return new_sightings, old_sightings
-
-
-def get_likelihoods(sightings: list[Sighting]) -> dict[int: float]:
-    # For each sighting, there's a moment within the last 15 minutes where the sighting tells us the current location
-    # of the crop circle with 100% accuracy. Calculate that location, and the number of seconds since that moment.
-    current_dt = datetime.now()
-    sightings_plus_moment_info = []
-    for sighting in sightings:
-        seconds_since_sighting = int((current_dt - sighting.dt).total_seconds())
-        seconds_since_moment = seconds_since_sighting % FIFTEEN_MINUTES
-        location_at_moment = (sighting.location + int(seconds_since_sighting / FIFTEEN_MINUTES)) % len(LOCATIONS)
-        sightings_plus_moment_info.append(
-            {'sighting': sighting, 'location': location_at_moment, 'seconds': seconds_since_moment}
-        )
-
-    # Sort the sightings by seconds_since_moment. The sighting with the lowest seconds_since_moment is the one
-    # that's most useful to us. The current location is either there or in the next location.
-    sightings_plus_moment_info = list(sorted(sightings_plus_moment_info, key=(lambda s: s['seconds'])))
-
-    # When there's more than one location, we know that the crop circle is at the later location (i.e. the location
-    # given by the sighting with the lowest seconds_since_moment). If there's only one location then there's some
-    # uncertainty (the crop circle might be at that location or it might be at the next location), and we calculate
-    # a likelihood based on the window of time that's unaccounted for by our sightings.
-    location_count = len(set(s['location'] for s in sightings_plus_moment_info))
-    if location_count > 1:
-        likelihoods = {
-            sightings_plus_moment_info[0]['location']: 1.0
-        }
-    else:
-        lowest_seconds_since_moment = sightings_plus_moment_info[0]['seconds']
-        highest_seconds_since_moment = sightings_plus_moment_info[-1]['seconds']
-        unknown_window = FIFTEEN_MINUTES - (highest_seconds_since_moment - lowest_seconds_since_moment)
-        likelihoods = {
-            sightings_plus_moment_info[0]['location']:
-                1 - lowest_seconds_since_moment / unknown_window,
-            (sightings_plus_moment_info[0]['location'] + 1) % len(LOCATIONS):
-                lowest_seconds_since_moment / unknown_window
-        }
-
-    return likelihoods
+    return most_accurate_window, old_sightings
