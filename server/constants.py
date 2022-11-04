cccf6ef9ee120ac0795f47bc1f7e96eeedbdc591
mattjrumble <mattjrumble@gmail.com>
2022-11-04 20:16:29 +0000

    Tweak ESTIMATED_SERVER_LAG_RATE

--- server/constants.py
+++ server/constants.py
@@ -33,7 +33,7 @@ GAME_UPDATE_DAY = WEDNESDAY
 GAME_UPDATE_TIME = time(hour=11, minute=30)
 GAME_UPDATE_GRACE_PERIOD_SECONDS = 30 * 60
 
-ESTIMATED_SERVER_LAG_RATE = 5 / FIFTEEN_MINUTES  # Assume up to 5 seconds of server lag every 15 minutes
+ESTIMATED_SERVER_LAG_RATE = (5 * 60) / (24 * 60 * 60)  # Assume up to 5 minutes of server lag every day
 SERVER_LAG_LIMIT = FIFTEEN_MINUTES
 
 API_KEY = 'gnomechild123'
