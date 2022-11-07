9ed62fc044cf05487196a023bffca1682947305e
mattjrumble <mattjrumble@gmail.com>
2022-11-07 22:34:52 +0000

    Change SERVER_LAG_LIMIT from 15 minutes to 60 minutes

--- server/constants.py
+++ server/constants.py
@@ -34,7 +34,7 @@ GAME_UPDATE_TIME = time(hour=11, minute=30)
 GAME_UPDATE_GRACE_PERIOD_SECONDS = 30 * 60
 
 ESTIMATED_SERVER_LAG_RATE = (10 * 60) / (24 * 60 * 60)  # Assume up to 10 minutes of server lag every day
-SERVER_LAG_LIMIT = FIFTEEN_MINUTES
+SERVER_LAG_LIMIT = 60 * 60
 
 API_KEY = 'gnomechild123'
 
